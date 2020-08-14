5e6687a330e2d96eea0441211c7b69efde07d063
Farbod Abolhassani <farbod.ab@hotmail.ca>
2020-08-14 14:30:14 -0400

    intial commit

---
+++ app/routes.py
@@ -0,0 +1,39 @@
+from app import app
+from flask import request
+import pandas as pd
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+@app.route('/individual')
+def individual():
+    stat = request.args.get('stat')
+    loc = request.args.get('loc')
+    date = request.args.get('date')
+    after = request.args.get('after')
+    before = request.args.get('before')
+    version = request.args.get('version')
+    return "Hello, World!"
+
+@app.route('/timeseries')
+def timeseries():
+    stat = request.args.get('stat')
+    loc = request.args.get('loc')
+    date = request.args.get('date')
+    after = request.args.get('after')
+    before = request.args.get('before')
+    version = request.args.get('version')
+    return "Hello, World!"
+
+@app.route('/summary')
+def summary():
+    loc = request.args.get('loc')
+    date = request.args.get('date')
+    version = request.args.get('version')
+    return "Hello, World!"
+
+@app.route('/version')
+def version():
+    return "Hello, World!"
